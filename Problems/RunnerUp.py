946aea02323cf55194c9fef96b5b84511e56e27c
Vidya C <59405528+VidyaCKabber@users.noreply.github.com>
2023-09-05 00:49:06 +0530

    Create RunnerUp.py

---
+++ Problems/RunnerUp.py
@@ -0,0 +1,28 @@
+# Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given  scores. Store them in a list and find the score of the runner-up.
+
+# Input Format
+
+# The first line contains . The second line contains an array   of  integers each separated by a space.
+
+# Constraints
+
+# Output Format
+
+# Print the runner-up score.
+
+# Sample Input 0
+
+# 5
+# 2 3 6 6 5
+# Sample Output 0
+
+# 5
+# Explanation 0
+
+# Given list is . The maximum score is , second maximum is . Hence, we print  as the runner-up score.
+
+n = int(input())
+arr = list(set(map(int, input().split())))
+arr.sort()
+runner_up = arr[-2]
+print(runner_up)
