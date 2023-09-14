52207fb87ef504c7bb2bc70c42e97011ab83e994
Vidya C <59405528+VidyaCKabber@users.noreply.github.com>
2023-09-14 22:29:22 +0530

    Create Competative_Coding.py

---
+++ Problems/Competative_Coding.py
@@ -0,0 +1,15 @@
+array = [1, 1, 2, 3, 4, 5]
+target = 10
+
+
+result = []
+for j in range(len(array)):
+    add = 0 
+    count = 0
+    for i in array[j:]:
+        if add != target and add < target:
+            add +=i
+            count+=1
+    if(add == target):
+        result.append(count)
+print(max(result))
