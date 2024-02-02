d1b0207b751a909ac3d495b2781726e06b7cc59b
Vidya C <59405528+VidyaCKabber@users.noreply.github.com>
2024-02-02 16:25:27 +0530

    Create Lower_to_upper_vs.py

---
+++ Problems/Lower_to_upper_vs.py
@@ -0,0 +1,39 @@
+# You are given a string and your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa.
+
+# For Example:
+
+# Www.HackerRank.com → wWW.hACKERrANK.COM
+# Pythonist 2 → pYTHONIST 2  
+# Function Description
+
+# Complete the swap_case function in the editor below.
+
+# swap_case has the following parameters:
+
+# string s: the string to modify
+# Returns
+
+# string: the modified string
+# Input Format
+
+# A single line containing a string .
+
+# Constraints
+
+
+# Sample Input 0
+
+# HackerRank.com presents "Pythonist 2".
+# Sample Output 0
+
+# hACKERrANK.COM PRESENTS "pYTHONIST 2".
+
+
+def swap_case(s):
+    res = ''.join([char.upper() if char.islower() else char.lower() if char.isupper() else char for char in s])
+    return res
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
