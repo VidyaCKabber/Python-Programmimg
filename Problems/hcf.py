b4b0f518595aa06e4b9715b25fedc2c90791b679
Vidya C <59405528+VidyaCKabber@users.noreply.github.com>
2024-05-02 19:54:08 +0530

    Update hcf.py

--- Problems/hcf.py
+++ Problems/hcf.py
@@ -11,3 +11,20 @@ while divider > 1:
         divider = divider - 1
     
 print(res)
+
+
+
+======2nd solution=======
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+max_num = a if a<b else b
+
+if a%b==0 and b%a==0:
+    hcf = max_num
+else:   
+    while(max_num > 1):
+        if a%max_num==0 and b%max_num==0:
+            hcf = max_num
+            break
+        max_num -=1
+    print(hcf)
