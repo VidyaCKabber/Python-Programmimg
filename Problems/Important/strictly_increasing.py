7f619534fa9c0da02321acffaa32ec02c767da73
Vidya C <59405528+VidyaCKabber@users.noreply.github.com>
2024-05-03 15:10:24 +0530

    Create strictly_increasing.py

---
+++ Problems/Important/strictly_increasing.py
@@ -0,0 +1,8 @@
+# Given a 0-indexed integer array nums, return true if it can be made strictly increasing after removing exactly one element, or false otherwise. If the array is already strictly increasing, return true.
+ 
+# The array nums is strictly increasing if nums[i - 1] < nums[i] for each index (1 <= i < nums.length).
+ 
+# Example:
+# Input: nums = [1,2,10,5,7] Output: true
+# Input: nums = [2,3,1,2] Output: false
+# Input: nums = [1,1,1] Output: false
