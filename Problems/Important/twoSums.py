2a0a08b1aa8bd2e7834858f3b5c024c2c8304fe6
Vidya C <59405528+VidyaCKabber@users.noreply.github.com>
2024-05-04 16:14:06 +0530

    Update twoSums.py

--- Problems/Important/twoSums.py
+++ Problems/Important/twoSums.py
@@ -25,7 +25,7 @@ class Solution:
         for i in range(len(nums)-1):
             if (nums[i]+nums[i+1]) == target:
                 return [i, i+1]
-        return -1
+        return []
 
 nums = input()
 target = input()
