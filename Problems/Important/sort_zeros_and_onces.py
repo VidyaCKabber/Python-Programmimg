12ef139b752eeba81ba95bf1e61f8044cd34fe23
Vidya C <59405528+VidyaCKabber@users.noreply.github.com>
2024-05-11 22:02:57 +0530

    Update sort_zeros_and_onces.py

--- Problems/Important/sort_zeros_and_onces.py
+++ Problems/Important/sort_zeros_and_onces.py
@@ -4,9 +4,11 @@ lst = [1, 1, 0, 0,1, 1, 0, 1, 1]
 count_of_zeros = lst.count(0)
 
 for v in range(count_of_zeros):
-    lst[v] = 0
+    if lst[v] == 1:
+        lst[v] = 0
     
 for j in range(count_of_zeros, len(lst)):
-    lst[j] = 1
+    if lst[j] == 0:
+        lst[j] = 1
     
 print(lst)
