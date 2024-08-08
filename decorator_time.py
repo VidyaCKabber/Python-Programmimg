feadfa8d4b999cc2f60af38f19d6c324718ffd45
Vidya C <59405528+VidyaCKabber@users.noreply.github.com>
2024-08-08 13:58:11 +0530

    Create decorator_time.py

---
+++ decorator_time.py
@@ -0,0 +1,21 @@
+import time
+
+def decorator(func):
+    def wrapper():
+        str_time = time.time()
+        print(f"Start time {str_time}")
+        func()
+        end_time = time.time()
+        print(f"Start time {end_time}")
+        
+        time_taken = end_time - str_time
+        return time_taken
+    return  wrapper
+    
+@decorator
+def myFun():
+    time.sleep(1)
+    print("hello !")
+
+a = myFun()
+print(a)
