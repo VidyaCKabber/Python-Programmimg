f2c079e44872d521e465b06cfe2b3f6b85ad966f
Vidya C <59405528+VidyaCKabber@users.noreply.github.com>
2024-08-15 23:26:27 +0530

    Create Missing_number_in_array.py

---
+++ Forward/List/Missing_number_in_array.py
@@ -0,0 +1,32 @@
+# Missing Number
+
+# Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+# Example 1:
+
+# Input: nums = [3,0,1]
+# Output: 2
+# Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+# Example 2:
+
+# Input: nums = [0,1]
+# Output: 2
+# Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+# Example 3:
+
+# Input: nums = [9,6,4,2,3,5,7,0,1]
+# Output: 8
+# Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        # use sum of natural numbers formula
+        n = len(nums)
+        expected_sum = n * ( n + 1) // 2
+        actual_sum = sum(nums)
+
+        return expected_sum - actual_sum 
+
+nums = [9,6,4,2,3,5,7,0,1]
+obj = Solution()
+print(obj.missingNumber(nums)
