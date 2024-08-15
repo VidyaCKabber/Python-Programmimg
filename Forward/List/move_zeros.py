dbc1b7b72e1b49412c219af326f1fb0129309291
Vidya C <59405528+VidyaCKabber@users.noreply.github.com>
2024-08-15 21:52:35 +0530

    Create move_zeros.py

---
+++ Forward/List/move_zeros.py
@@ -0,0 +1,48 @@
+# Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+# Note that you must do this in-place without making a copy of the array.
+# Example 1:
+
+# Input: nums = [0,1,0,3,12]
+# Output: [1,3,12,0,0]
+# Example 2:
+
+# Input: nums = [0]
+# Output: [0]
+ 
+
+# Constraints:
+# 1 <= nums.length <= 104
+# -231 <= nums[i] <= 231 - 1
+ 
+
+# Follow up: Could you minimize the total number of operations done?
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # time complexity is O(n^2)
+        # zeros_count = nums.count(0)
+
+        # for i in range(zeros_count):
+        #     nums.remove(0)
+
+        # nums.extend([0 for i in range(zeros_count)])
+        # print(nums)
+
+
+         # time complexity is O(n)
+        non_zero_index = 0
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[non_zero_index] = nums[i]
+                non_zero_index += 1
+
+        for j in range(non_zero_index, len(nums)):
+            nums[j] = 0
+        
+        print(nums)
+        
+        
