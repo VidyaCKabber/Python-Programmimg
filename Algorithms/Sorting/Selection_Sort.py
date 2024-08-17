60b9ca950a43a11f78a316feac712eed0aff4ebe
Vidya C <59405528+VidyaCKabber@users.noreply.github.com>
2024-08-17 11:48:45 +0530

    Update Selection_Sort.py

--- Algorithms/Sorting/Selection_Sort.py
+++ Algorithms/Sorting/Selection_Sort.py
@@ -16,7 +16,7 @@ class Solution:
             arr[i] = min
         return arr
 
-if __name__ = 'main()':
+if __name__ = '__main__':
     t = int(input())
     for _ in range(t);
         n = int(input())
