694544bbb212613014f64e977cccfa58caf02d2f
Vidya C <59405528+VidyaCKabber@users.noreply.github.com>
2024-09-23 09:38:16 +0530

    Create TwoSum.py

---
+++ Questions/TwoSum.py
@@ -0,0 +1,54 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+ 
+# Example 1:
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ 
+# Example 2:
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+ 
+# Example 3:
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+ 
+# Constraints:
+# 2 <= nums.length <= 10^4
+# -10^9 <= nums[i] <= 10^9
+# -10^9 <= target <= 10^9
+
+
+
+
+
+
+
+
+def target_value(nums, target):
+    hash_table = {}
+    for index, num in enumerate(nums):
+        diff = target - num
+
+        if diff in hash_table:
+            return [hash_table[diff], index]
+        else:
+            hash_table.update({num: index})
+
+nums = [2,7,11,15]
+target = 9 
+result = target_value(nums, target)
+print(result)
+
+nums = [3,2,4]
+target = 6
+
+result = target_value(nums, target)
+print(result)
+
+nums = [3,3]
+target = 6
+result = target_value(nums, target)
+print(result)
