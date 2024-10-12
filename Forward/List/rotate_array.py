6180ae8b37b7fc9dcbc009136c33cbe3a5fd5d13
Vidya C <59405528+VidyaCKabber@users.noreply.github.com>
2024-10-12 17:08:23 +0530

    Update rotate_array.py

--- Forward/List/rotate_array.py
+++ Forward/List/rotate_array.py
@@ -36,6 +36,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        if len(nums) == 0 or len(nums) == k:
+           return nums
         k = k % len(nums)
         nums[:] = nums[-k:] + nums[:-k]
 
