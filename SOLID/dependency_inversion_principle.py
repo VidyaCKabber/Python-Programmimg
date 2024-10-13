63536b5cc41d99e37559681e5dd7e6ea25c97733
Vidya C <59405528+VidyaCKabber@users.noreply.github.com>
2024-10-13 23:26:46 +0530

    Update dependency_inversion_principle.py

--- SOLID/dependency_inversion_principle.py
+++ SOLID/dependency_inversion_principle.py
@@ -1,38 +1,52 @@
 # High-level modules should not depend on low-level modules. Both should depend on abstractions.
 
-# Bad example
-class Worker:
-    def work(self):
-        pass
+from abc import ABC, abstractmethod
 
-    def eat(self):
-        pass
-
-class Robot(Worker):
-    def work(self):
-        print("Robot is working")
-
-    def eat(self):
-        raise NotImplementedError("Robot can't eat")
-
-# Good example
-class Workable(ABC):
+# Abstract Interface (Abstraction)
+class Switchable(ABC):
     @abstractmethod
-    def work(self):
+    def turn_on(self):
         pass
-
-class Eatable(ABC):
+    
     @abstractmethod
-    def eat(self):
+    def turn_off(self):
         pass
 
-class Human(Workable, Eatable):
-    def work(self):
-        print("Human is working")
+# Low-level module
+class LightBulb(Switchable):
+    def turn_on(self):
+        print("Light Bulb is on")
+        
+    def turn_off(self):
+        print("Light Bulb is off")
 
-    def eat(self):
-        print("Human is eating")
+# Another low-level module (New type of bulb)
+class Fan(Switchable):
+    def turn_on(self):
+        print("Fan is on")
+    
+    def turn_off(self):
+        print("Fan is off")
 
-class Robot(Workable):
-    def work(self):
-        print("Robot is working")
+# High-level module
+class Switch:
+    def __init__(self, device: Switchable):  # Depends on abstraction
+        self.device = device
+    
+    def operate(self, state: str):
+        if state == "ON":
+            self.device.turn_on()
+        elif state == "OFF":
+            self.device.turn_off()
+
+# Usage
+bulb = LightBulb()
+fan = Fan()
+
+switch = Switch(bulb)
+switch.operate("ON")  # Output: Light Bulb is on
+switch.operate("OFF")  # Output: Light Bulb is off
+
+switch = Switch(fan)
+switch.operate("ON")  # Output: Fan is on
+switch.operate("OFF")  # Output: Fan is off
